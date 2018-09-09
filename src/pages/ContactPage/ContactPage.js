79406115772facb9dd3e36b0fb6835ac99157b99
Josh Klimaszewski <joshklimaszewski@Joshs-MacBook-Pro.local>
2018-09-09 14:44:37 -0400

    test commit

--- src/pages/ContactPage/ContactPage.js
+++ src/pages/ContactPage/ContactPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './style.css';
 import Nav from '../../components/Nav';
-
+// test commit
 const ContactPage = () => {
     return (
         <div className="contact-page">
