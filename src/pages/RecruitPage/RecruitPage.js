6eebf8a14ee3c9b3f5bd5e196d935973fe262630
Josh Klimaszewski <joshklimaszewski@Joshs-MacBook-Pro.local>
2018-09-09 15:43:24 -0400

    links in recruit page

--- src/pages/RecruitPage/RecruitPage.js
+++ src/pages/RecruitPage/RecruitPage.js
@@ -1,66 +1,339 @@
-import React from 'react';
-import './style.css';
-import {
-    toElement as scrollToElement
-} from '../../utils/scroll';
-import Nav from '../../components/Nav';
+import React, { Component } from "react";
+import "./style.css";
+import { toElement as scrollToElement } from "../../utils/scroll";
 
-
-const RecruitPage = () => {
+class RecruitPage extends Component {
+  scrollToPage = pageSelector => {
+    const nextPage = document.querySelector(pageSelector);
+    scrollToElement(nextPage);
+  };
+  render() {
     return (
-        <div className="recruit-page">
+      <div className="recruit-page">
         <h1>Recruitment Solutions</h1>
         <h5>"Don't expect tactics to solve a strategic problem."</h5>
-        <p className="paragraph">This mantra drives our day to day activity on behalf of our clients. Artemis
-                Search Group continues to deliver on critical personnel needs for distinct and
-                key reasons: </p>
-
-               <li> <img src="assets/images/artemisbullet.png" className="bullet"  width="18px" height="18px"/>We are selective in our client relationships and are experts in their industries.</li>
-               <li> <img src="assets/images/artemisbullet.png" className="bullet" width="18px" height="18px"/>We are methodical in applying our process.</li>
-               <li> <img src="assets/images/artemisbullet.png" className="bullet" width="18px" height="18px"/>We work directly with executive leadership to understand the metrics of long-term excellent performance.</li>
-               <li> <img src="assets/images/artemisbullet.png" className="bullet" width="18px" height="18px"/>We encourage clients to seek aspects of: Fit, Character and Performance, as these are the true indicators of success.</li>
-
+        <p className="paragraph">
+          This mantra drives our day to day activity on behalf of our clients.
+          Artemis Search Group continues to deliver on critical personnel needs
+          for distinct and key reasons:{" "}
+        </p>
+        <li>
+          {" "}
+          <img
+            src="assets/images/artemisbullet.png"
+            className="bullet"
+            width="18px"
+            height="18px"
+          />
+          We are selective in our client relationships and are experts in their
+          industries.
+        </li>
+        <li>
+          {" "}
+          <img
+            src="assets/images/artemisbullet.png"
+            className="bullet"
+            width="18px"
+            height="18px"
+          />
+          We are methodical in applying our process.
+        </li>
+        <li>
+          {" "}
+          <img
+            src="assets/images/artemisbullet.png"
+            className="bullet"
+            width="18px"
+            height="18px"
+          />
+          We work directly with executive leadership to understand the metrics
+          of long-term excellent performance.
+        </li>
+        <li>
+          {" "}
+          <img
+            src="assets/images/artemisbullet.png"
+            className="bullet"
+            width="18px"
+            height="18px"
+          />
+          We encourage clients to seek aspects of: Fit, Character and
+          Performance, as these are the true indicators of success.
+        </li>
         <h5>Critical Questions regarding your talent brand.</h5>
-        <li> <img src="assets/images/artemisbullet.png" className="bullet"  width="18px" height="18px"/>Do you currently have a resume key-word search tool driving your hiring inputs?</li>
-        <li> <img src="assets/images/artemisbullet.png" className="bullet"  width="18px" height="18px"/>Are YOUR top performers looking for work on The Job Boards? Why would you assume this of your competitor’s top performers?</li>
-        <li> <img src="assets/images/artemisbullet.png" className="bullet"  width="18px" height="18px"/>Are the qualities that define your strongest player, visible on their resume?</li>
-        <li> <img src="assets/images/artemisbullet.png" className="bullet"  width="18px" height="18px"/>When you were forced to make a personnel change, were the flaws in that team member found on their resume?</li>
-        <li> <img src="assets/images/artemisbullet.png" className="bullet"  width="18px" height="18px"/>Is your talent team telling you that there is a skill-set shortage?</li>
-
+        <li>
+          {" "}
+          <img
+            src="assets/images/artemisbullet.png"
+            className="bullet"
+            width="18px"
+            height="18px"
+          />
+          Do you currently have a resume key-word search tool driving your
+          hiring inputs?
+        </li>
+        <li>
+          {" "}
+          <img
+            src="assets/images/artemisbullet.png"
+            className="bullet"
+            width="18px"
+            height="18px"
+          />
+          Are YOUR top performers looking for work on The Job Boards? Why would
+          you assume this of your competitor’s top performers?
+        </li>
+        <li>
+          {" "}
+          <img
+            src="assets/images/artemisbullet.png"
+            className="bullet"
+            width="18px"
+            height="18px"
+          />
+          Are the qualities that define your strongest player, visible on their
+          resume?
+        </li>
+        <li>
+          {" "}
+          <img
+            src="assets/images/artemisbullet.png"
+            className="bullet"
+            width="18px"
+            height="18px"
+          />
+          When you were forced to make a personnel change, were the flaws in
+          that team member found on their resume?
+        </li>
+        <li>
+          {" "}
+          <img
+            src="assets/images/artemisbullet.png"
+            className="bullet"
+            width="18px"
+            height="18px"
+          />
+          Is your talent team telling you that there is a skill-set shortage?
+        </li>
         <h5>Our Process</h5>
-        <p className="paragraph">Artemis Search Group has an uncommon perspective on recruiting and talent
-                    acquisition. We work directly with organizational leadership as Opportunity
-                    Sales Agents (OSA). We actively represent outstanding opportunities within
-                    vibrant, growing organizations to the highest performing industry professionals.</p>
-                        <li><img src="assets/images/artemisbullet.png" className="bullet" width="18px" height="18px"/>Conduct Opportunity design consulting with PARTNER.</li>
-                        <li> <img src="assets/images/artemisbullet.png" className="bullet" width="18px" height="18px"/> Identify Opportunity Sales Team Champion</li>
-                        <li> <img src="assets/images/artemisbullet.png" className="bullet" width="18px" height="18px"/> Develop firm project timeline and pre-arrange dates with team</li>
-                        <li> <img src="assets/images/artemisbullet.png" className="bullet" width="18px" height="18px"/> Develop firm project timeline and pre-arrange dates with team</li>
-                        <li> <img src="assets/images/artemisbullet.png" className="bullet" width="18px" height="18px"/> Develop firm project timeline and pre-arrange dates with team</li>
-                        <li><img src="assets/images/artemisbullet.png" className="bullet" width="18px" height="18px"/> Develop in-depth understanding of Opportunity performance criteria</li>
-                        <li> <img src="assets/images/artemisbullet.png" className="bullet" width="18px" height="18px"/> Develop Opportunity Summary and verbal brochure with PARTNER</li>
-                        <li> <img src="assets/images/artemisbullet.png" className="bullet" width="18px" height="18px"/> Discuss competitors, associated value-chain(s), jointly identify possible candidates</li>
-                        <li> <img src="assets/images/artemisbullet.png" className="bullet" width="18px" height="18px"/> Identify and attract targeted professionals</li>
-                        <li> <img src="assets/images/artemisbullet.png" className="bullet" width="18px" height="18px"/> Conduct in-depth interviews utilizing behavioral based interviewing techniques</li>
-                        <li> <img src="assets/images/artemisbullet.png" className="bullet" width="18px" height="18px"/> Present most committed professionals to PARTNER for arranged interviews</li>
-                        <li> <img src="assets/images/artemisbullet.png" className="bullet" width="18px" height="18px"/> Consult with candidate(s) prior to interview</li>
-                        <li><img src="assets/images/artemisbullet.png" className="bullet" width="18px" height="18px"/> Consult with PARTNER prior to interview</li>
-                        <li> <img src="assets/images/artemisbullet.png" className="bullet" width="18px" height="18px"/> Close candidate on offer acceptance threshold</li>
-                        <li> <img src="assets/images/artemisbullet.png" className="bullet" width="18px" height="18px"/> Counsel candidate on perils of counteroffer/minimize PARTNER vulnerability</li>
-                        <li> <img src="assets/images/artemisbullet.png" className="bullet" width="18px" height="18px"/> Simultaneous offer extension and acceptance gaining start date commitment</li>
-                        <li> <img src="assets/images/artemisbullet.png" className="bullet" width="18px" height="18px"/> Assist candidate with completing all pre-employment contingencies</li>
-                        <li> <img src="assets/images/artemisbullet.png" className="bullet" width="18px" height="18px"/> Follow-up with PARTNER and candidate on and after start date</li>
-    <p className="paragraph"> Skilled executives and trained business development professionals may see striking similarity to a formalized sales process.  If you recognized this, you are our type of Partner.  
-                            
-                            Artemis Search Group distinguishes itself from the competition with this philosophical perspective.  We outshine our competitors because our process mirrors this belief system.  If your Talent Acquisition Process hasn’t been the vanguard of your sales effort, you are missing opportunity to grow your organization.  Let us show you how!  We won’t let you down.  Let’s get to work!</p>
-
-            <h5>Executive Search</h5>
-            <a className="joblink1">Hire A Vet</a>
-            <a className="joblink">Recruitment Process In-sourcing</a>
-    
-            
+        <p className="paragraph">
+          Artemis Search Group has an uncommon perspective on recruiting and
+          talent acquisition. We work directly with organizational leadership as
+          Opportunity Sales Agents (OSA). We actively represent outstanding
+          opportunities within vibrant, growing organizations to the highest
+          performing industry professionals.
+        </p>
+        <li>
+          <img
+            src="assets/images/artemisbullet.png"
+            className="bullet"
+            width="18px"
+            height="18px"
+          />
+          Conduct Opportunity design consulting with PARTNER.
+        </li>
+        <li>
+          {" "}
+          <img
+            src="assets/images/artemisbullet.png"
+            className="bullet"
+            width="18px"
+            height="18px"
+          />{" "}
+          Identify Opportunity Sales Team Champion
+        </li>
+        <li>
+          {" "}
+          <img
+            src="assets/images/artemisbullet.png"
+            className="bullet"
+            width="18px"
+            height="18px"
+          />{" "}
+          Develop firm project timeline and pre-arrange dates with team
+        </li>
+        <li>
+          {" "}
+          <img
+            src="assets/images/artemisbullet.png"
+            className="bullet"
+            width="18px"
+            height="18px"
+          />{" "}
+          Develop firm project timeline and pre-arrange dates with team
+        </li>
+        <li>
+          {" "}
+          <img
+            src="assets/images/artemisbullet.png"
+            className="bullet"
+            width="18px"
+            height="18px"
+          />{" "}
+          Develop firm project timeline and pre-arrange dates with team
+        </li>
+        <li>
+          <img
+            src="assets/images/artemisbullet.png"
+            className="bullet"
+            width="18px"
+            height="18px"
+          />{" "}
+          Develop in-depth understanding of Opportunity performance criteria
+        </li>
+        <li>
+          {" "}
+          <img
+            src="assets/images/artemisbullet.png"
+            className="bullet"
+            width="18px"
+            height="18px"
+          />{" "}
+          Develop Opportunity Summary and verbal brochure with PARTNER
+        </li>
+        <li>
+          {" "}
+          <img
+            src="assets/images/artemisbullet.png"
+            className="bullet"
+            width="18px"
+            height="18px"
+          />{" "}
+          Discuss competitors, associated value-chain(s), jointly identify
+          possible candidates
+        </li>
+        <li>
+          {" "}
+          <img
+            src="assets/images/artemisbullet.png"
+            className="bullet"
+            width="18px"
+            height="18px"
+          />{" "}
+          Identify and attract targeted professionals
+        </li>
+        <li>
+          {" "}
+          <img
+            src="assets/images/artemisbullet.png"
+            className="bullet"
+            width="18px"
+            height="18px"
+          />{" "}
+          Conduct in-depth interviews utilizing behavioral based interviewing
+          techniques
+        </li>
+        <li>
+          {" "}
+          <img
+            src="assets/images/artemisbullet.png"
+            className="bullet"
+            width="18px"
+            height="18px"
+          />{" "}
+          Present most committed professionals to PARTNER for arranged
+          interviews
+        </li>
+        <li>
+          {" "}
+          <img
+            src="assets/images/artemisbullet.png"
+            className="bullet"
+            width="18px"
+            height="18px"
+          />{" "}
+          Consult with candidate(s) prior to interview
+        </li>
+        <li>
+          <img
+            src="assets/images/artemisbullet.png"
+            className="bullet"
+            width="18px"
+            height="18px"
+          />{" "}
+          Consult with PARTNER prior to interview
+        </li>
+        <li>
+          {" "}
+          <img
+            src="assets/images/artemisbullet.png"
+            className="bullet"
+            width="18px"
+            height="18px"
+          />{" "}
+          Close candidate on offer acceptance threshold
+        </li>
+        <li>
+          {" "}
+          <img
+            src="assets/images/artemisbullet.png"
+            className="bullet"
+            width="18px"
+            height="18px"
+          />{" "}
+          Counsel candidate on perils of counteroffer/minimize PARTNER
+          vulnerability
+        </li>
+        <li>
+          {" "}
+          <img
+            src="assets/images/artemisbullet.png"
+            className="bullet"
+            width="18px"
+            height="18px"
+          />{" "}
+          Simultaneous offer extension and acceptance gaining start date
+          commitment
+        </li>
+        <li>
+          {" "}
+          <img
+            src="assets/images/artemisbullet.png"
+            className="bullet"
+            width="18px"
+            height="18px"
+          />{" "}
+          Assist candidate with completing all pre-employment contingencies
+        </li>
+        <li>
+          {" "}
+          <img
+            src="assets/images/artemisbullet.png"
+            className="bullet"
+            width="18px"
+            height="18px"
+          />{" "}
+          Follow-up with PARTNER and candidate on and after start date
+        </li>
+        <p className="paragraph">
+          {" "}
+          Skilled executives and trained business development professionals may
+          see striking similarity to a formalized sales process. If you
+          recognized this, you are our type of Partner. Artemis Search Group
+          distinguishes itself from the competition with this philosophical
+          perspective. We outshine our competitors because our process mirrors
+          this belief system. If your Talent Acquisition Process hasn’t been the
+          vanguard of your sales effort, you are missing opportunity to grow
+          your organization. Let us show you how! We won’t let you down. Let’s
+          get to work!
+        </p>
+        <h5>Executive Search</h5>
+        <div
+          className="joblink1"
+          onClick={() => this.scrollToPage(".hire-vet-page")}
+        >
+          Hire A Vet
         </div>
+
+        <div
+          className="joblink"
+          onClick={() => this.scrollToPage(".insource-page")}
+        >
+          Recruitment Process In-sourcing
+        </div>
+      </div>
     );
+  }
 }
 
 export default RecruitPage;
